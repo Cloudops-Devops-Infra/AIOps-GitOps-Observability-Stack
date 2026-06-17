318d3764ddaa0f3e377bb52821125bfc39b1e63f
Subash Patra <patrasubash92@gmail.com>
2026-06-17 18:29:32 +0530

    Fix local S3 routing by enabling s3_use_path_style

--- main.tf
+++ main.tf
@@ -19,6 +19,9 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  
+  # THE GOLDEN FIX: Force path-style addressing for local mock environments
+  s3_use_path_style           = true 
 
   endpoints {
     s3 = var.aws_endpoint
